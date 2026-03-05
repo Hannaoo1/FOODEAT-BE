cf337e7580b9cb6fdaff4e5f6437e3aaa7a3c775
Hannaoo1 <hannaj5494@gmail.com>
2026-03-05 10:13:10 +0900

    chore: 프로젝트 초기 설정

---
+++ src/main/java/com/lgcns/foodeat/FoodeatApplication.java
@@ -0,0 +1,13 @@
+package com.lgcns.foodeat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FoodeatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FoodeatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lgcns/foodeat/FoodeatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lgcns.foodeat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodeatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
