2ceed4351f3d8d174b65c9e990c9d25188ec3bf3
Hannaoo1 <hannaj5494@gmail.com>
2026-03-11 22:13:36 +0900

    feat: 메뉴 레포지토리 작성

--- src/main/java/com/lgcns/foodeat/domain/foodti/repository/MenuRepository.java
+++ src/main/java/com/lgcns/foodeat/domain/foodti/repository/MenuRepository.java
@@ -2,8 +2,15 @@ package com.lgcns.foodeat.domain.foodti.repository;
 
 import com.lgcns.foodeat.domain.foodti.entity.Menu;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface MenuRepository extends JpaRepository<Menu, Long> {
-    List<Menu> findByFoodtiNumber(Integer foodtiNumber);
+    @Query(value = "SELECT * FROM menus WHERE foodti_number = :foodtiNumber ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    List<Menu> findRandomByFoodtiNumber(@Param("foodtiNumber") Integer foodtiNumber, @Param("limit") int limit);
+
+    @Query(value = "SELECT * FROM menus ORDER BY RAND() LIMIT :limit", nativeQuery = true)
+    List<Menu> findRandomMenus(@Param("limit") int limit);
 }
