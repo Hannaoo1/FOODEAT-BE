36c1f7f745b00ef9d0726103be93463dec5c67df
Hannaoo1 <hannaj5494@gmail.com>
2026-03-14 18:32:22 +0900

    chore: 스웨거 설명 추가

--- src/main/java/com/lgcns/foodeat/domain/image/dto/response/ImageUploadResponse.java
+++ src/main/java/com/lgcns/foodeat/domain/image/dto/response/ImageUploadResponse.java
@@ -1,13 +1,17 @@
 package com.lgcns.foodeat.domain.image.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.util.List;
 
+@Schema(description = "이미지 업로드 응답")
 @Getter
 @AllArgsConstructor
 public class ImageUploadResponse {
+
+    @Schema(description = "업로드된 이미지 URL 목록", example = "[\"https://foodeat-images.s3.ap-northeast-2.amazonaws.com/diary/uuid.jpg\"]")
     private List<String> imageUrls;
 
     public static ImageUploadResponse of(List<String> imageUrls) {
